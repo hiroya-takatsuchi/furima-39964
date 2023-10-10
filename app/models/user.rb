72f8fc9e7dce89e6099cada53c268e2037bb069b
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-10-10 12:15:12 +0900

    バリデーションの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
       
-         validates :nickname, presence: true
-         validates :last_name, presence: true
-         validates :first_name, presence: true
-         validates :kana_last_name, presence: true
-         validates :kana_first_name, presence: true
+         validates :nickname, presence: true, length: { minimum: 6 }
+         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角文字で入力してください" }
+         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角文字で入力してください" }
+         validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
+         validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
          validates :birthday, presence: true
+         validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
+         validates :password, presence: true, length: { minimum: 6 }, , format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/, message: "must include at least one letter and one digit" }
+         validates :password, confirmation: true
+         validates :password_confirmation, presence: true
+
 
 end
