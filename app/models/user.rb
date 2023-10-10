4fb49a3f02831d16cf7942646f9d40b38a5fd11c
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-10-10 15:34:11 +0900

    ログイン、ログアウト、エラーメッセージ

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
       
-         validates :nickname, presence: true, length: { minimum: 6 }
+         validates :nickname, presence: true
          validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角文字で入力してください" }
          validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "は全角文字で入力してください" }
          validates :kana_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
          validates :kana_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: "は全角カタカナで入力してください" }
          validates :birthday, presence: true
          validates :email, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-         validates :password, presence: true, length: { minimum: 6 }, , format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/, message: "must include at least one letter and one digit" }
-         validates :password, confirmation: true
-         validates :password_confirmation, presence: true
+         validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/, message: "must include at least one letter and one digit" }
+       
 
 
 end
