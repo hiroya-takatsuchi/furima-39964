b9ebc6db0f5cfe2215e89702af5b39d9c2d4c58d
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-10-12 14:37:24 +0900

    itemのバリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,20 @@
 class ItemsController < ApplicationController
   def index
-    
+    @items = Item.all
   end
 
   def new
-    
+    @item = Item.new
   end
+  
+  def create
+    Items.create(item_params)
+    redirect_to '/'
+  end
+
+  private
+  def tweet_params
+    params.require(:item).permit(:name, :image, :text)
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
-  
+  validates :item, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :status_id, presence: true
+  validates :delivery_charge_id, presence: true
+  validates :shipping_area_id, presence: true
+  validates :shipping_day_id, presence: true
+  validates :price, presence: true
 end
