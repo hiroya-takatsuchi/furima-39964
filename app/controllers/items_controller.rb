f44fbf6bb992ded39a28ffa1af81befc461c217b
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-10-13 17:39:26 +0900

    商品購入画面、データ保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,10 +8,12 @@ class ItemsController < ApplicationController
   end
   
   def create
-    items = Items.all
+    Item.create(item_params)
     redirect_to '/'
   end
 
-  
-
-end
+  private
+  def item_params
+    params.require(:item).permit(:image, :item, :explanation, :price, :category_id, :status_id, :delivery_charge_id, :shipping_area_id, :shipping_day_id).merge(user_id: current_user.id)
+  end
+end  
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,5 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   validates :item, presence: true
   validates :explanation, presence: true
   validates :category_id, presence: true
@@ -7,4 +8,12 @@ class Item < ApplicationRecord
   validates :shipping_area_id, presence: true
   validates :shipping_day_id, presence: true
   validates :price, presence: true
+
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :shipping_area
+  belongs_to :shipping_day
+  belongs_to :status
+  has_one_attached :image
 end
