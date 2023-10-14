c8ba22b14e1614be5c05a32feddccdc2c5e70a8d
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-10-14 15:08:29 +0900

    avtiveshashのバリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,16 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  validates :image, presence: true
   validates :item, presence: true
   validates :explanation, presence: true
-  validates :category_id, presence: true
-  validates :status_id, presence: true
-  validates :delivery_charge_id, presence: true
-  validates :shipping_area_id, presence: true
-  validates :shipping_day_id, presence: true
   validates :price, presence: true
+  validates :price, format: { with: /\A([3-9][0-9]{2}|[1-9][0-9]{3,6}|10000000)\z/ }
+  validates :price, numericality: { greater_than: 0 }
+  validates :category, numericality: { other_than: 1 }
+  validates :status, numericality: { other_than: 1 }
+  validates :delivery_charge, numericality: { other_than: 1 }
+  validates :shipping_area, numericality: { other_than: 1 }
+  validates :shipping_day, numericality: { other_than: 1 }
 
   belongs_to :category
   belongs_to :status
