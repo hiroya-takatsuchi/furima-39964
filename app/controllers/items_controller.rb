3ca35c78a06a4b5ec1224fadc4c42f3fbbd23641
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-10-15 17:36:42 +0900

    show画面への遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,6 +17,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end  
+
   private
   def item_params
     params.require(:item).permit(:image, :item, :explanation, :price, :category_id, :status_id, :delivery_charge_id, :shipping_area_id, :shipping_day_id).merge(user_id: current_user.id)
