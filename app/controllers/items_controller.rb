3beedeb57efd169533a570d572a3fab18deee90d
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-10-16 14:24:55 +0900

    商品情報編集機の実施

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :correct_user, only: [:edit, :update]
+  
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -19,10 +21,31 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find(params[:id])
-  end  
+  end 
+
+  def edit
+    @item = Item.find(params[:id])
+  end 
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def correct_user
+    @item = Item.find(params[:id])
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
 
   private
   def item_params
     params.require(:item).permit(:image, :item, :explanation, :price, :category_id, :status_id, :delivery_charge_id, :shipping_area_id, :shipping_day_id).merge(user_id: current_user.id)
   end
+
 end  
