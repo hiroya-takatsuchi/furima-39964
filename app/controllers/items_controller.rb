253c4e70c7224acf6ed3afeadbf4c5bc6db91c49
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-10-16 16:15:49 +0900

    destroyの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
-  before_action :correct_user, only: [:edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update, :destroy]
   
   def index
     @items = Item.all.order('created_at DESC')
@@ -35,10 +35,13 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    item = Item.find(params[:id])
-    item.destroy
-    redirect_to root_path
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :show, status: :unprocessable_entity
+    end
   end
+
 
 
   
