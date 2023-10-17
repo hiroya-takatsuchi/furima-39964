0778c6ffd17d3504c5ab1cb449e3b7b1f44657d7
hiroya-takatsuchi <h.takatsuchi1020@gmail.com>
2023-10-17 20:32:03 +0900

    formオブジェクトの修正

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,14 +1,13 @@
 class PurchaseShippingAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :shipping_area_id, :city, :street, :building, :telephone, :purchase_id
+  attr_accessor :post_code, :shipping_area_id, :city, :street, :building, :telephone, :purchase_id, :user_id, :item_id
 
   with_options presence: true do
-    validates :purchase_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :shipping_area_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :street
     validates :telephone
   end
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+  validates :shipping_area_id, numericality: {other_than: 0, message: "can't be blank"}
 end
--- spec/models/purchase_shipping_address_spec.rb
+++ spec/models/purchase_shipping_address_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe PurchaseShippingAddress, type: :model do
   describe '購入情報の保存' do
     before do
       user = FactoryBot.create(:user)
-      item = FactoryBot.create(:item, user: user)
-      @purchase = FactoryBot.build(:purchase, user_id: user.id, item_id: item.id)
+      item = FactoryBot.create(:item)
+      @purchase_shipping_address = FactoryBot.build(:purchase_shipping_address, user_id: user.id, item_id: item.id)
     end
 
     context '内容に問題ない場合' do
       it 'すべての値が正しく入力されていれば保存できること' do
-        expect(@purchase).to be_valid
+        expect(@purchase_shipping_address).to be_valid
       end
       it 'buildingは空でも保存できること' do
         @purchase_shipping_address.building = nil
@@ -20,19 +20,20 @@ RSpec.describe PurchaseShippingAddress, type: :model do
 
     context '内容に問題がある場合' do
       it 'post_codeが空だと保存できないこと' do
-        @purchase.post_code = ''
-        @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Postcode can't be blank")
+        @purchase_shipping_address.post_code = ''
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Post code can't be blank")
       end
       it 'post_codeが半角のハイフンを含んだ正しい形式でないと保存できないこと' do
-        @purchase.post_code = '1111111'
-        @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Postcode can't be blank")
+        @purchase_shipping_address.post_code = '1111111'
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Post code is invalid. Include hyphen(-)")
       end
-      it '発送元の地域に「---」が選択されている場合は出品できない' do
-        @purchase.shipping_area_id = '1'
-        @purchase.valid?
-        expect(@purchase.errors.full_messages).to include("Shipping area can't be blank")
+      it '発送の地域に「---」が選択されている場合は出品できない' do
+        @purchase_shipping_address.shipping_area_id = '1'
+        @purchase_shipping_address.valid?
+        expect(@purchase_shipping_address.errors.full_messages).to include("Shipping area can't be blank")
+      end  
       it 'priceが空だと保存できないこと' do
       end
       it 'priceが全角数字だと保存できないこと' do
